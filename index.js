35783e1ef8bbc91d673853d8bcaad46d62b1c955
123siddharthmandal <siddharthmandal525@gmail.com>
2026-03-11 10:29:03 +0530

    changes in index.js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Dockerized Node.js App!");
+  res.send("Hello from Dockerized Node.js App! new changes added to file index.js");
 });
 
 app.listen(PORT, () => {
